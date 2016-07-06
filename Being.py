38484bafb1474bc133df20121c31994abd1e41c6
Piotr Świątek <p_swiatek@mensa.org.pl>
2016-07-06 11:12:35 +0200

    Create Being.py

---
+++ Being.py
@@ -0,0 +1,2 @@
+class Being():
+    def __init__(self, race, name, current_career, previous_careers, age, gender, secondary_presonal_details, profile_first_line, profile_second_line, trappings, weapon, armour)
