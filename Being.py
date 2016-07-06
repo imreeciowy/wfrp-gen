7c635f9bafd8423a7eb856ac8b176f969880dabb
Piotr Świątek <p_swiatek@mensa.org.pl>
2016-07-06 11:52:07 +0200

    Update Being.py

--- Being.py
+++ Being.py
@@ -1,2 +1,20 @@
 class Being():
-    def __init__(self, race, name, current_career, previous_careers, age, gender, secondary_presonal_details, profile_first_line, profile_second_line, trappings, weapon, armour)
+    def __init__(self, race, name, current_career, previous_careers, age, gender, secondary_presonal_details, profile_first_line, profile_second_line, trappings, weapon, armour, skills, talents, money, spells):
+        self.race = race
+        self.name = name
+        self.current_career = current_career
+        self.previous_careers = previous_careers
+        self.age = age
+        self.gender = gender
+        self.secondary_presonal_details = secondary_presonal_details
+        self.profile_first_line = profile_first_line
+        self.profile_second_line = profile_second_line
+        self.trappings = trappings
+        self.weapon = weapon
+        self.armour = armour
+        self.skills = skills
+        self.talents = talents
+        self.money = money
+        self.spells = spells
+        
+        
