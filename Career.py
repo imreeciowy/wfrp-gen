25056e74093f01d68af14277da6089903b617ee6
Piotr Świątek <p_swiatek@mensa.org.pl>
2016-07-07 11:38:36 +0200

    Create Career.py

---
+++ Career.py
@@ -0,0 +1,8 @@
+class Career:
+    def __init__(career_name, advances, skills_to_take, talents_to_take, career_trappings, race_dependent)
+        self.career_name = career_name
+        self.advances = advances
+        self.skills_to_take = skills_to_take
+        self.talents_to_take = talents_to_take
+        self.career_trappings = career_trappings
+        self.race_dependent = race_dependent
