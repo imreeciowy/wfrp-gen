647ba69e17f55c382063d529ec369e65fe7704da
Piotr Świątek <p_swiatek@mensa.org.pl>
2016-07-10 02:04:20 +0200

    class issues - it is now better, slightly better

--- generator0.0.7.py
+++ generator0.0.7.py
@@ -17,7 +17,7 @@ def x_dices_n(x,n):
     return result
 
 #crude race selector ;)
-player = Being.Human()
+player = Being.Human([], [])
 print(player.base_line_1)
 # roll for stats with generic dice
 fresh_stats=[]
@@ -84,10 +84,9 @@ print(*chosen_stats, sep='\t')
 #print(*fresh_stats, sep='\t')
 
 # increment race base with chosen stats
-body1 = [sum(x) for x in zip(base1, chosen_stats)]
-# TODO # neatstat_upper_line = 
+player.body1 = [sum(x) for x in zip(player.base_line_1, chosen_stats)]
 print(*stat_first_names, sep='\t')
-print(*body1, sep='\t')
+print(*player.body1, sep='\t')
 
 # save to file
 time_string = time.strftime("%Y-%m-%d--%H%M%S")
@@ -106,7 +105,7 @@ for D in chosen_stats:
     f.write(str(D))
     f.write('\t')
 f.write('\n')
-for A in body1:
+for A in player.body1:
     f.write(str(A))
     f.write('\t')
 f.close()
