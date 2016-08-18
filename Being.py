7f68fb35491ee5bbaa951d3c140c1006a8d245b7
Piotr Świątek <p_swiatek@mensa.org.pl>
2016-08-18 11:25:34 +0200

    Update Being.py

--- Being.py
+++ Being.py
@@ -1,26 +1,11 @@
 class Being():
     def __init__(self, base_line_1, body1, base_line_2, body2, wounds_table, fate_table):
-    # def __init__(self, race, name, current_career, previous_careers, age, gender, secondary_presonal_details, base_line_1, base_line_2, trappings, weapon, armour, skills, talents, money, spells):
-        # self.race = race
-        # self.name = name
-        # self.current_career = current_career
-        # self.previous_careers = previous_careers
-        # self.age = age
-        # self.gender = gender
-        # self.secondary_presonal_details = secondary_presonal_details
         self.base_line_1 = base_line_1
         self.body1 = body1
         self.base_line_2 = base_line_2
         self.body2 = body2
         self.wounds_table = wounds_table
         self.fate_table = fate_table
-        # self.trappings = trappings
-        # self.weapon = weapon
-        # self.armour = armour
-        # self.skills = skills
-        # self.talents = talents
-        # self.money = money
-        # self.spells = spells
 
 
 class Dwarf(Being):
